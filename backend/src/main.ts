579b7bdad98c128c05a52516856c0691aa3b174a
Georgi Krastev <gkrastev9715@abv.bg>
2019-11-12 17:33:04 +0200

    back end initial commit

---
+++ backend/src/main.ts
@@ -0,0 +1,12 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { SystemErrorFilter } from './common/filters/system-error.filter';
+import helmet from 'helmet';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.use(helmet());
+  app.useGlobalFilters(new SystemErrorFilter());
+  await app.listen(3000);
+}
+bootstrap();
