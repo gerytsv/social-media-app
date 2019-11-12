684c01380179781ef4b339991ea519f45be62a22
Georgi Krastev <gkrastev9715@abv.bg>
2019-11-12 17:37:13 +0200

    adding tranformation interceptor

---
+++ backend/src/transformer/decorators/publish.ts
@@ -0,0 +1,19 @@
+// tslint:disable: only-arrow-functions
+import 'reflect-metadata';
+
+export const Publish = <T>(dto?: T) =>
+  function(target: any, propertyKey: string) {
+    const exposed = Reflect.getMetadata('dto:transformer', target) || [];
+    if (dto) {
+      exposed.push({
+        key: propertyKey,
+        dto,
+      });
+    } else {
+      exposed.push({
+        key: propertyKey,
+        dto: null,
+      });
+    }
+    Reflect.defineMetadata('dto:transformer', exposed, target);
+  };
