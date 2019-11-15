ac248891d349c13b758f5197e5fc61b4140562f8
Georgi Krastev <gkrastev9715@abv.bg>
2019-11-15 16:25:23 +0200

    adding authgard

---
+++ front-end/src/app/common/auth/auth.guard.ts
@@ -0,0 +1,31 @@
+import { NotificatorService } from '../../core/services/notificator.service';
+import { AuthService } from '../../core/services/auth.service';
+import { Injectable } from '@angular/core';
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot
+} from '@angular/router';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(
+    private readonly authService: AuthService,
+    private readonly notificator: NotificatorService
+  ) {}
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean> {
+    return this.authService.isLoggedIn$.pipe(
+      tap((loggedIn) => {
+        if (!loggedIn) {
+          this.notificator.error(`You not authorized to access this page!`);
+        }
+      })
+    );
+  }
+}
