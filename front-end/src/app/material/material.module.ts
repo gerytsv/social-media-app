e1e9a70657114afe207a210feaf24db000f799b4
Georgi Krastev <gkrastev9715@abv.bg>
2019-11-15 16:27:10 +0200

    adding basic material needs

---
+++ front-end/src/app/material/material.module.ts
@@ -0,0 +1,54 @@
+import { NgModule, Optional, SkipSelf } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  MatButtonModule,
+  MatListModule,
+  MatToolbarModule,
+  MatCardModule,
+  MatDividerModule,
+  MatFormFieldModule,
+  MatTabsModule,
+  MatSidenavModule,
+  MatIconModule,
+  MatInputModule,
+} from '@angular/material';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatListModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatDividerModule,
+    MatFormFieldModule,
+    MatTabsModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatInputModule,
+  ],
+  exports: [
+    MatButtonModule,
+    MatListModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatDividerModule,
+    MatFormFieldModule,
+    MatTabsModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatInputModule,
+  ]
+})
+export class MaterialModule {
+
+  constructor(@Optional() @SkipSelf() parent: MaterialModule) {
+    if (parent) {
+      throw new Error(`MaterialModule has already been initialized!`);
+    }
+  }
+
+}
