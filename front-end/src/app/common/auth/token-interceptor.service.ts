e32f12bef841f559768b8505e4dff8bc759bfe31
Georgi Krastev <gkrastev9715@abv.bg>
2019-11-15 16:25:37 +0200

    adding token interceptor

---
+++ front-end/src/app/common/auth/token-interceptor.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpInterceptor,
+  HttpRequest,
+  HttpHandler
+} from '@angular/common/http';
+import { StorageService } from '../../core/services/storage.service';
+
+@Injectable()
+export class TokenInterceptorService implements HttpInterceptor {
+  constructor(private readonly storageService: StorageService) {}
+
+  intercept(request: HttpRequest<any>, next: HttpHandler) {
+    const token = this.storageService.read('token') || '';
+    const updatedRequest = request.clone({
+      headers: request.headers.set('Authorization', `Bearer ${token}`)
+    });
+
+    return next.handle(updatedRequest);
+  }
+}
