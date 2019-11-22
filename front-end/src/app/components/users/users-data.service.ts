98c376d9ba86d8d6a961174f2f6c6807daf7ac6b
Georgi Krastev <gkrastev9715@abv.bg>
2019-11-22 16:59:03 +0200

    added users-data service methods

--- front-end/src/app/components/users/users-data.service.ts
+++ front-end/src/app/components/users/users-data.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ShowUserInfoDTO } from './models/show-user-info.dto';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { ShowDetailedInfoDTO } from './models/show-detailed-info.dto';
 
 @Injectable({
   providedIn: 'root'
@@ -10,8 +11,8 @@ export class UsersDataService {
   public constructor(private readonly http: HttpClient) {}
 
 
-  public getUserById(id: string): Observable<ShowUserInfoDTO> {
-    return this.http.get<ShowUserInfoDTO>(`http://localhost:3000/api/users/${id}`);
+  public getUserByUsername(username: string): Observable<ShowDetailedInfoDTO> {
+    return this.http.get<ShowDetailedInfoDTO>(`http://localhost:3000/api/users/${username}`);
   }
 
   public updateProfilePic(file: any): Observable<any> {
@@ -22,5 +23,13 @@ export class UsersDataService {
 
   public updateProfileInfo(data: any): Observable<any> {
     return this.http.put<any>('http://localhost:3000/api/users/account', data);
+  }
+
+  public followUser(id: string): Observable<ShowUserInfoDTO> {
+    return this.http.post<ShowUserInfoDTO>(`http://localhost:3000/api/follow/users/${id}`, null);
+  }
+
+  public unfollowUser(id: string): Observable<ShowUserInfoDTO> {
+    return this.http.delete<ShowUserInfoDTO>(`http://localhost:3000/api/follow/users/${id}`);
   }
 }
