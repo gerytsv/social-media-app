6ed16138ddc0132518256d2a3de2941e7892ee31
Georgi Krastev <gkrastev9715@abv.bg>
2019-11-22 17:00:06 +0200

    changes in profile component

--- front-end/src/app/components/users/profile-info/profile.component.ts
+++ front-end/src/app/components/users/profile-info/profile.component.ts
@@ -5,6 +5,8 @@ import { ShowUserInfoDTO } from '../models/show-user-info.dto';
 import { User } from '../models/user';
 import { Subscription } from 'rxjs';
 import { AuthService } from '../../../core/services/auth.service';
+import { NotificatorService } from '../../../core/services/notificator.service';
+import { ShowDetailedInfoDTO } from '../models/show-detailed-info.dto';
 
 @Component({
   selector: 'app-profile-info',
@@ -14,30 +16,36 @@ import { AuthService } from '../../../core/services/auth.service';
 export class ProfileInfoComponent implements OnInit {
   private loggedInSubscription: Subscription;
   private userSubscription: Subscription;
+  private routeSubscription: Subscription;
 
   public isOwner = false;
   public loggedIn: boolean;
   public loggedInUser: User;
-  public user: ShowUserInfoDTO;
-  public copyOfUser: ShowUserInfoDTO;
+  public user: ShowDetailedInfoDTO;
+  public copyOfUser: ShowDetailedInfoDTO;
   public showEditMenu = false;
+  public params: string;
+  public followed = true;
 
   constructor(
     private readonly activatedRoute: ActivatedRoute,
     private readonly usersDataService: UsersDataService,
-    private readonly authService: AuthService
+    private readonly authService: AuthService,
+    private readonly notificator: NotificatorService
   ) {}
 
   public ngOnInit() {
-    // tslint:disable-next-line: no-string-literal
-    const userId: string = this.activatedRoute.snapshot.params['id'];
-    this.usersDataService.getUserById(userId).subscribe(res => {
-      this.user = res;
-      this.copyOfUser = { ...res };
-      this.userSubscription = this.authService.loggedUser$.subscribe(user => {
-        if (this.user.username === user.username) {
-          this.isOwner = true;
-        }
+    this.routeSubscription = this.activatedRoute.params.subscribe(res => {
+      this.params = res.username;
+      this.usersDataService.getUserByUsername(this.params).subscribe(response => {
+        this.user = response;
+        this.followed = this.user.followers.some(item => this.user.username !== item.username);
+        this.copyOfUser = { ...response };
+        this.userSubscription = this.authService.loggedUser$.subscribe(user => {
+          if (this.user.username === user.username) {
+            this.isOwner = true;
+          }
+        });
       });
     });
 
@@ -47,11 +55,35 @@ export class ProfileInfoComponent implements OnInit {
   }
 
   public updateProfileView(data: ShowUserInfoDTO) {
-    this.user = data;
+    this.user = { ...this.user, ...data };
     this.toggleEditMenu();
   }
 
   public toggleEditMenu() {
     this.showEditMenu = !this.showEditMenu;
+  }
+
+  public followUser() {
+    try {
+      this.usersDataService
+        .followUser(this.user.id)
+        .subscribe(res => {
+          this.notificator.success(`${this.user.username} has been followed`);
+          this.followed = true;
+        });
+    } catch {
+      this.notificator.success(`Something went wrong`);
+    }
+  }
+
+  public unfollowUser() {
+    try {
+      this.usersDataService.unfollowUser(this.user.id).subscribe(res => {
+        this.notificator.success(`${this.user.username} has been unfollowed`);
+        this.followed = false;
+      });
+    } catch {
+      this.notificator.success(`Something went wrong`);
+    }
   }
 }
