647d3cb9ae0039f2a9ef3349c0dcae3d9ddeaac8
Gergana Tsvetkova <gerytsv@gmail.com>
2019-11-25 10:35:17 +0200

    Adding resolvers in the core module

--- front-end/src/app/core/core.module.ts
+++ front-end/src/app/core/core.module.ts
@@ -1,27 +1,28 @@
-import { NgModule, Optional, SkipSelf } from '@angular/core';
-import { AuthGuard } from '../common/auth/auth.guard';
-
-import { AuthService } from './services/auth.service';
-
-import { NotificatorService } from './services/notificator.service';
-
-import { StorageService } from './services/storage.service';
+import { AllPostsResolver } from "./resolvers/all-posts.resolver";
+import { NewsfeedResolver } from "./resolvers/newsfeed.resolver";
+import { NgModule, Optional, SkipSelf } from "@angular/core";
+import { AuthGuard } from "../common/auth/auth.guard";
+import { AuthService } from "./services/auth.service";
+import { NotificatorService } from "./services/notificator.service";
+import { StorageService } from "./services/storage.service";
 
 @NgModule({
-    declarations: [],
-    imports: [],
-    providers: [
-      AuthGuard,
-      AuthService,
-      NotificatorService,
-      StorageService,
-    ],
-    exports: []
-  })
-  export class CoreModule {
-    constructor(@Optional() @SkipSelf() parent: CoreModule) {
-        if (parent) {
-          throw new Error(`CoreModule has already been initialized!`);
-        }
-      }
+  declarations: [],
+  imports: [],
+  providers: [
+    AuthGuard,
+    AuthService,
+    NotificatorService,
+    StorageService,
+    NewsfeedResolver,
+    AllPostsResolver
+  ],
+  exports: []
+})
+export class CoreModule {
+  constructor(@Optional() @SkipSelf() parent: CoreModule) {
+    if (parent) {
+      throw new Error(`CoreModule has already been initialized!`);
+    }
   }
+}
