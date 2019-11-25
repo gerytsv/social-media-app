ab375acbd3a2e89d8b6fcfea2f47192463709de1
Gergana Tsvetkova <gerytsv@gmail.com>
2019-11-25 08:55:57 +0200

    enabled CORS

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,6 +5,7 @@ import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.use(helmet());
   app.useGlobalFilters(new SystemErrorFilter());
   await app.listen(3000);
