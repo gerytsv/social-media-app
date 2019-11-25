763de8bbe1d0445af20d1b1e3bbc77e367d3279b
Gergana Tsvetkova <gerytsv@gmail.com>
2019-11-25 10:33:37 +0200

    Adding posts module

---
+++ front-end/src/app/post/posts.module.ts
@@ -0,0 +1,14 @@
+import { PostPreviewComponent } from "./../post-preview/post-preview.component";
+import { CoreModule } from "./../core/core.module";
+import { PostsDataService } from "./services/posts-data.service";
+import { PostComponent } from "./post.component";
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+
+@NgModule({
+  declarations: [PostComponent, PostPreviewComponent],
+  imports: [CommonModule],
+  providers: [PostsDataService],
+  exports: [PostComponent, PostPreviewComponent]
+})
+export class PostsModule {}
