919a7ba9fdf0f339b34d9e119a9145c20a60679c
Gergana Tsvetkova <gerytsv@gmail.com>
2019-11-25 10:33:28 +0200

    Adding posts component

---
+++ front-end/src/app/post/post.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit, Input } from "@angular/core";
+import { PostDTO } from "./models/post.dto";
+
+@Component({
+  selector: "app-post",
+  templateUrl: "./post.component.html",
+  styleUrls: ["./post.component.css"]
+})
+export class PostComponent implements OnInit {
+  @Input() public post: PostDTO;
+  constructor() {}
+
+  ngOnInit() {}
+}
