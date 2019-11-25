ee891da60f0218bdb38792122b429da148f86259
Gergana Tsvetkova <gerytsv@gmail.com>
2019-11-25 10:36:22 +0200

    Adding modules in the app module

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,28 +1,30 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule } from './material/material.module';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { CoreModule } from './core/core.module';
-import { ReactiveFormsModule, FormsModule } from '@angular/forms';
-import { ToastrModule } from 'ngx-toastr';
-import { JwtModule } from '@auth0/angular-jwt';
-import { TokenInterceptorService } from './common/auth/token-interceptor.service';
+import { AppRoutingModule } from "./app-routing.module";
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MaterialModule } from "./material/material.module";
+import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
+import { CoreModule } from "./core/core.module";
+import { ReactiveFormsModule, FormsModule } from "@angular/forms";
+import { ToastrModule } from "ngx-toastr";
+import { JwtModule } from "@auth0/angular-jwt";
+import { TokenInterceptorService } from "./common/auth/token-interceptor.service";
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
+    CoreModule,
+    AppRoutingModule,
     BrowserModule,
     BrowserAnimationsModule,
     FormsModule,
     MaterialModule,
     HttpClientModule,
-    CoreModule,
     ReactiveFormsModule,
     ToastrModule.forRoot({
       timeOut: 3000,
-      positionClass: 'toast-top-right',
+      positionClass: "toast-top-right",
       preventDuplicates: true,
       countDuplicates: true
     }),
