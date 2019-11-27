7b1da37971bc25bc6bbdc208f9095ba4981be18d
Georgi Krastev <gkrastev9715@abv.bg>
2019-11-27 17:03:15 +0200

    updating app routing module

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,30 +1,36 @@
-import { NgModule } from "@angular/core";
-import { Routes, RouterModule } from "@angular/router";
-import { AuthGuard } from "./common/auth/auth.guard";
-import { NewsfeedComponent } from "./newsfeed/newsfeed.component";
-import { NewsfeedResolver } from "./core/resolvers/newsfeed.resolver";
-import { HomepageComponent } from "./components/homepage/homepage.component";
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from './common/auth/auth.guard';
+import { NewsfeedComponent } from './newsfeed/newsfeed.component';
+import { NewsfeedResolver } from './core/resolvers/newsfeed.resolver';
+import { HomepageComponent } from './components/homepage/homepage.component';
 
 const routes: Routes = [
-  { path: "", redirectTo: "home", pathMatch: "full" },
+  { path: '', redirectTo: 'homepage', pathMatch: 'full' },
   {
-    path: "home",
-    loadChildren: () =>
-      import("./../app/newsfeed/newsfeed.module").then(m => m.NewsfeedModule)
-  },
-  {
-    path: "explore",
-    loadChildren: () =>
-      import("./../app/explore/explore.module").then(m => m.ExploreModule)
-  },
-  {
-    path: "users",
+    path: 'home',
     canActivate: [AuthGuard],
     loadChildren: () =>
-      import("./components/users/users.module").then(m => m.UsersModule)
+      import('./../app/newsfeed/newsfeed.module').then(m => m.NewsfeedModule)
   },
   {
-    path: "homepage",
+    path: 'explore',
+    loadChildren: () =>
+      import('./../app/explore/explore.module').then(m => m.ExploreModule)
+  },
+  {
+    path: 'admin',
+    loadChildren: () =>
+      import('./../app/components/admin-history/admin-history.module').then(m => m.AdminHistoryModule)
+  },
+  {
+    path: 'users',
+    canActivate: [AuthGuard],
+    loadChildren: () =>
+      import('./components/users/users.module').then(m => m.UsersModule)
+  },
+  {
+    path: 'homepage',
     component: HomepageComponent
   }
 ];
