20a22cc7cebd208678347628039ec57c3faafbe9
Georgi Krastev <gkrastev9715@abv.bg>
2019-11-27 17:06:25 +0200

    added loged in subscription in app component

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AuthService } from './core/services/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'front-end';
+
+  private loggedInSubscription: Subscription;
+  public loggedIn = false;
+
+  constructor(
+    private readonly authService: AuthService,
+  ) {}
+
+  ngOnInit() {
+    this.loggedInSubscription = this.authService.isLoggedIn$.subscribe( res => {
+      this.loggedIn = res;
+    });
+  }
+
+  ngOnDestroy() {
+    this.loggedInSubscription.unsubscribe();
+  }
 }
