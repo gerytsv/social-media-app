7d56f73f8ca8fb25aad862e2803a56c71522f549
Georgi Krastev <gkrastev9715@abv.bg>
2019-11-27 17:18:08 +0200

    added minimized user module

---
+++ front-end/src/app/shared/minimized-user/minimized-user.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { User } from '../../components/users/models/user';
+import { ShowDetailedInfoDTO } from '../../components/users/models/show-detailed-info.dto';
+
+@Component({
+  selector: 'app-minimized-user',
+  templateUrl: './minimized-user.component.html',
+  styleUrls: ['./minimized-user.component.css']
+})
+export class MinimizedUserComponent implements OnInit {
+
+  @Input() public user: ShowDetailedInfoDTO;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  public get profileLink() {
+    return ['/users', this.user.username];
+  }
+
+}
