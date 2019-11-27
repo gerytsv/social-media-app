6bfae5d42dddc7042f5f8a37dacdec43ae7f30d5
Georgi Krastev <gkrastev9715@abv.bg>
2019-11-27 17:15:17 +0200

    added email field in update profile component

--- front-end/src/app/components/users/profile-info/update-profile/update-profile.component.ts
+++ front-end/src/app/components/users/profile-info/update-profile/update-profile.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { UsersDataService } from '../../users-data.service';
 import { NotificatorService } from '../../../../core/services/notificator.service';
 import { ShowUserInfoDTO } from '../../models/show-user-info.dto';
+import { ConformationDialogBoxComponent } from '../../../../shared/conformation-dialog-box/conformation-dialog-box.component';
+import { DialogService } from '../../../../core/services/dialog.service';
 
 @Component({
   selector: 'app-update-profile',
@@ -10,45 +12,59 @@ import { ShowUserInfoDTO } from '../../models/show-user-info.dto';
 })
 export class UpdateProfileComponent implements OnInit {
   @Input() public user: ShowUserInfoDTO;
-  @Output() public updatedUser: EventEmitter<ShowUserInfoDTO> = new EventEmitter();
+  @Output() public updatedUser: EventEmitter<
+    ShowUserInfoDTO
+  > = new EventEmitter();
 
   constructor(
     private readonly usersDataService: UsersDataService,
     private readonly notificator: NotificatorService,
+    private readonly dialog: DialogService
   ) {}
 
-  public ngOnInit() {
-  }
+  public ngOnInit() {}
 
   public updateProfilePic(data: any) {
     this.notificator.warn('Proceeding...');
-    this.usersDataService
-      .updateProfilePic(data)
-      .subscribe(res => {
-        try {
+    this.usersDataService.updateProfilePic(data).subscribe(res => {
+      try {
         this.user.avatarUrl = res.photoLink;
-        this.notificator.success('Photo uploaded, save changes to update profile');
-        } catch {
+        this.notificator.success(
+          'Photo uploaded, save changes to update profile'
+        );
+      } catch {
         this.notificator.error('Photo was not uploaded');
-        }
-      });
+      }
+    });
   }
 
   public updateProfileInfo() {
-    this.usersDataService.updateProfileInfo(this.user).subscribe(
-      res => {   this.user = {...this.user, ...res};
-      },
-      error => {
-        this.notificator.error('Something whent wrong');
-      },
-      () => {
-        this.notificator.success('Profile updated');
-      }
+    const confirmData = {
+      description: 'Do you want to save changes?'
+    };
+    const refDialog = this.dialog.openConfDialog(
+      ConformationDialogBoxComponent,
+      confirmData
     );
-  }
 
-  public emitUpdatedProfileInfo() {
-    this.updatedUser.emit(this.user);
+    refDialog.afterClosed().subscribe(result => {
+      if (result) {
+        this.usersDataService
+          .updateProfileInfo(this.user, this.user.id)
+          .subscribe(
+            res => {
+              this.user = { ...this.user, ...res };
+              this.updatedUser.emit(this.user);
+            },
+            error => {
+              this.notificator.error('Could not update profile');
+            },
+            () => {
+              this.notificator.success('Profile updated');
+            }
+          );
+      }
+    });
   }
 
 }
