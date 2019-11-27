613b9aa24ce8f6a6df77459e614cccd5a7f6f216
Gergana Tsvetkova <gerytsv@gmail.com>
2019-11-27 17:15:24 +0200

    Adding routes in app-routing.module

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,23 +1,22 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { AuthGuard } from "./common/auth/auth.guard";
-import { NewsfeedComponent } from "./components/newsfeed/newsfeed.component";
+import { NewsfeedComponent } from "./post/newsfeed/newsfeed.component";
 import { NewsfeedResolver } from "./core/resolvers/newsfeed.resolver";
 import { HomepageComponent } from "./components/homepage/homepage.component";
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 const routes: Routes = [
   { path: "", redirectTo: "home", pathMatch: "full" },
   {
     path: "home",
     loadChildren: () =>
-      import("./components/newsfeed/newsfeed.module").then(
-        m => m.NewsfeedModule
-      )
+      import("./post/newsfeed/newsfeed.module").then(m => m.NewsfeedModule)
   },
   {
     path: "explore",
     loadChildren: () =>
-      import("./components/explore/explore.module").then(m => m.ExploreModule)
+      import("./post/explore/explore.module").then(m => m.ExploreModule)
   },
   {
     path: "users",
@@ -28,7 +27,11 @@ const routes: Routes = [
   {
     path: "homepage",
     component: HomepageComponent
-  }
+  },
+
+  { path: "not-found", component: NotFoundComponent },
+
+  { path: "**", redirectTo: "/not-found" }
 ];
 
 @NgModule({
