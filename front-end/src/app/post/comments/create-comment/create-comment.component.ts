9dcbeb3022baffa410a140f09fd69c490ecbebda
Gergana Tsvetkova <gerytsv@gmail.com>
2019-12-05 14:04:22 +0200

    Adding create-comment component

---
+++ front-end/src/app/post/comments/create-comment/create-comment.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+
+@Component({
+    selector: 'app-create-comment',
+    templateUrl: './create-comment.component.html',
+    styleUrls: ['./create-comment.component.css'],
+})
+export class CreateCommentComponent implements OnInit {
+    public content = '';
+    public postId: string;
+    @Output() public bodyToSend: EventEmitter<{
+        content: string;
+    }> = new EventEmitter();
+
+    constructor() {}
+
+    public onPostButtonClick() {
+        this.bodyToSend.emit({ content: this.content });
+    }
+
+    ngOnInit() {}
+}
