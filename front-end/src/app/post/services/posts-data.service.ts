31f415e7df51bdb7fe044178b685106bc08f056f
Gergana Tsvetkova <gerytsv@gmail.com>
2019-12-05 14:11:37 +0200

    Adding upload photo method in posts-data.service

--- front-end/src/app/post/services/posts-data.service.ts
+++ front-end/src/app/post/services/posts-data.service.ts
@@ -1,36 +1,42 @@
-import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
-import { Observable } from "rxjs";
-import { PostDTO } from "../models/post.dto";
-import { CONFIG } from "../../config/config";
-import { CreatePostDTO } from "../models/create-post.dto";
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { PostDTO } from '../models/post.dto';
+import { CONFIG } from '../../config/config';
+import { CreatePostDTO } from '../models/create-post.dto';
 
 @Injectable({
-  providedIn: "root"
+    providedIn: 'root',
 })
 export class PostsDataService {
-  constructor(private readonly http: HttpClient) {}
+    constructor(private readonly http: HttpClient) {}
 
-  public allPosts(): Observable<any> {
-    return this.http.get<PostDTO>(`${CONFIG.DOMAIN_NAME}/posts`);
-  }
+    public allPosts(): Observable<any> {
+        return this.http.get<PostDTO>(`${CONFIG.DOMAIN_NAME}/posts`);
+    }
 
-  public followedPosts(): Observable<any> {
-    return this.http.get<PostDTO>(`${CONFIG.DOMAIN_NAME}/posts/feed`);
-  }
+    public followedPosts(): Observable<any> {
+        return this.http.get<PostDTO>(`${CONFIG.DOMAIN_NAME}/posts/feed`);
+    }
 
-  public getPostById(id: string): Observable<PostDTO> {
-    return this.http.get<PostDTO>(`${CONFIG.DOMAIN_NAME}/posts/${id}`);
-  }
+    public getPostById(id: string): Observable<PostDTO> {
+        return this.http.get<PostDTO>(`${CONFIG.DOMAIN_NAME}/posts/${id}`);
+    }
 
-  public createPost(newPost: CreatePostDTO): Observable<PostDTO> {
-    return this.http.post<PostDTO>(
-      `${CONFIG.DOMAIN_NAME}/posts/create`,
-      newPost
-    );
-  }
+    public uploadPhoto(base64: string): Observable<any> {
+        return this.http.post<any>(`${CONFIG.DOMAIN_NAME}/api/photos/post`, {
+            base64,
+        });
+    }
 
-  public deletePost(id: number): Observable<PostDTO> {
-    return this.http.delete<PostDTO>(`${CONFIG.DOMAIN_NAME}/posts/${id}`);
-  }
+    public createPost(newPost: CreatePostDTO): Observable<PostDTO> {
+        return this.http.post<PostDTO>(
+            `${CONFIG.DOMAIN_NAME}/posts/create`,
+            newPost
+        );
+    }
+
+    public deletePost(id: number): Observable<PostDTO> {
+        return this.http.delete<PostDTO>(`${CONFIG.DOMAIN_NAME}/posts/${id}`);
+    }
 }
