21924fb89d14a1e30c50948938ab21e0e54eda5c
Gergana Tsvetkova <gerytsv@gmail.com>
2019-12-05 14:08:37 +0200

    Adding upload-image component

---
+++ front-end/src/app/post/create-post/upload-image-post/upload-image-post.component.ts
@@ -0,0 +1,48 @@
+import { Component, ViewChild, Output, EventEmitter } from '@angular/core';
+import { ImageCropperComponent } from '../image-cropper/component/image-cropper.component';
+import { ImageCroppedEvent } from '../image-cropper/interfaces';
+
+@Component({
+    selector: 'app-upload-image-post',
+    templateUrl: './upload-image-post.component.html',
+    styleUrls: ['./upload-image-post.component.css'],
+})
+export class UploadImagePostComponent {
+    @Output() public bodyToSend: EventEmitter<any> = new EventEmitter();
+
+    imageChangedEvent: any = '';
+    croppedImage: any = '';
+    showCropper = false;
+
+    @ViewChild(ImageCropperComponent, null) imageCropper: ImageCropperComponent;
+
+    fileChangeEvent(event: any): void {
+        this.imageChangedEvent = event;
+    }
+    imageCropped(event: ImageCroppedEvent) {
+        this.croppedImage = event.base64;
+        this.bodyToSend.emit(this.croppedImage);
+    }
+    imageLoaded() {
+        this.showCropper = true;
+        console.log('Image loaded');
+    }
+    cropperReady() {
+        console.log('Cropper ready');
+    }
+    loadImageFailed() {
+        console.log('Load failed');
+    }
+    rotateLeft() {
+        this.imageCropper.rotateLeft();
+    }
+    rotateRight() {
+        this.imageCropper.rotateRight();
+    }
+    flipHorizontal() {
+        this.imageCropper.flipHorizontal();
+    }
+    flipVertical() {
+        this.imageCropper.flipVertical();
+    }
+}
