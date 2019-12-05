16d02061ee1689262ae7580f31bac4ed2a5ca8d2
Gergana Tsvetkova <gerytsv@gmail.com>
2019-12-05 14:02:45 +0200

    Adding missing components in posts module

--- front-end/src/app/post/posts.module.ts
+++ front-end/src/app/post/posts.module.ts
@@ -1,23 +1,41 @@
-import { PostDetailPreviewComponent } from "./post-detail-preview/post-detail-preview.component";
-import { LikesComponent } from "./likes/likes.component";
-import { PostPreviewComponent } from "./post-preview/post-preview.component";
-import { CoreModule } from "./../core/core.module";
-import { PostsDataService } from "./services/posts-data.service";
-import { PostComponent } from "./post.component";
-import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
-import { CommentsComponent } from './comments/comments.component';
+import { FormsModule } from '@angular/forms';
+import { CommentModule } from './comments/comment.module';
+import { PostsRoutingModule } from './posts-routing.module';
+import { LikesComponent } from './likes/likes.component';
+import { PostsDataService } from './services/posts-data.service';
+import { PostComponent } from './post.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UploadImagePostComponent } from './create-post/upload-image-post/upload-image-post.component';
+import { ImageCropperModule } from './create-post/image-cropper/image-cropper.module';
+import { CreatePostComponent } from './create-post/create-post.component';
+import { SinglePostResolver } from '../core/resolvers/single-post.resolver';
+import { PostPreviewComponent } from '../components/explore/post-preview/post-preview.component';
+import { PostDetailPreviewComponent } from '../components/explore/post-detail-preview/post-detail-preview.component';
 
 @NgModule({
-  declarations: [
-    PostComponent,
-    PostPreviewComponent,
-    PostDetailPreviewComponent,
-    LikesComponent,
-    CommentsComponent
-  ],
-  imports: [CommonModule],
-  providers: [PostsDataService],
-  exports: [PostComponent, PostPreviewComponent]
+    declarations: [
+        LikesComponent,
+        PostComponent,
+        UploadImagePostComponent,
+        CreatePostComponent,
+        PostPreviewComponent,
+        PostDetailPreviewComponent,
+    ],
+    imports: [
+        CommonModule,
+        ImageCropperModule,
+        PostsRoutingModule,
+        CommentModule,
+        FormsModule,
+    ],
+    providers: [PostsDataService, SinglePostResolver],
+    exports: [
+        PostComponent,
+        LikesComponent,
+        PostPreviewComponent,
+        PostDetailPreviewComponent,
+        CreatePostComponent,
+    ],
 })
 export class PostsModule {}
