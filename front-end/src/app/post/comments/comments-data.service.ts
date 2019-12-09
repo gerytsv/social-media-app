420d69c74f345c25d3c38da6ef11726b157c88b4
Gergana Tsvetkova <gerytsv@gmail.com>
2019-12-09 11:35:17 +0200

    Adding ppdate comment method

--- front-end/src/app/post/comments/comments-data.service.ts
+++ front-end/src/app/post/comments/comments-data.service.ts
@@ -5,30 +5,36 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
-    providedIn: 'root',
+  providedIn: 'root',
 })
 export class CommentsDataService {
-    constructor(private readonly http: HttpClient) {}
+  constructor(private readonly http: HttpClient) {}
 
-    public createComment(
-        postId: string,
-        body: { content: string }
-    ): Observable<CommentDTO> {
-        return this.http.post<CommentDTO>(
-            `${CONFIG.DOMAIN_NAME}/api/posts/${postId}/comments`,
-            body
-        );
-    }
+  public createComment(
+    postId: string,
+    body: { content: string }
+  ): Observable<CommentDTO> {
+    return this.http.post<CommentDTO>(
+      `${CONFIG.DOMAIN_NAME}/api/posts/${postId}/comments`,
+      body
+    );
+  }
 
-    public getCommentsOfPost(postId: string): Observable<CommentDTO[]> {
-        return this.http.get<any>(
-            `${CONFIG.DOMAIN_NAME}/api/comments/${postId}/comments`
-        );
-    }
+  public getCommentsOfPost(postId: string): Observable<CommentDTO[]> {
+    return this.http.get<any>(
+      `${CONFIG.DOMAIN_NAME}/api/comments/${postId}/comments`
+    );
+  }
 
-    public deleteComment(commentId: string): Observable<{ message: string }> {
-        return this.http.delete<{ message: string }>(
-            `${CONFIG.DOMAIN_NAME}/api/posts/comments/${commentId}`
-        );
-    }
+  public deleteComment(commentId: string): Observable<{ message: string }> {
+    return this.http.delete<{ message: string }>(
+      `${CONFIG.DOMAIN_NAME}/api/posts/comments/${commentId}`
+    );
+  }
+
+  public updateCommentContent(commentId: string, body: { content: string }) {
+    this.http.put<CommentDTO>(
+      `${CONFIG.DOMAIN_NAME}/api/posts/comments/${commentId}`, body
+    );
+  }
 }
