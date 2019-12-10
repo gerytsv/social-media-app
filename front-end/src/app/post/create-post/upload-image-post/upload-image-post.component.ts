30dbd78e3ea1c42d38ca65a68ff30491e43b457d
Georgi Krastev <gkrastev9715@abv.bg>
2019-12-10 17:44:28 +0200

    adding file upload validation

--- front-end/src/app/post/create-post/upload-image-post/upload-image-post.component.ts
+++ front-end/src/app/post/create-post/upload-image-post/upload-image-post.component.ts
@@ -1,45 +1,61 @@
 import { Component, ViewChild, Output, EventEmitter } from '@angular/core';
 import { ImageCropperComponent } from '../image-cropper/component/image-cropper.component';
 import { ImageCroppedEvent } from '../image-cropper/interfaces';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { NotificatorService } from '../../../core/services/notificator.service';
 
 @Component({
-    selector: 'app-upload-image-post',
-    templateUrl: './upload-image-post.component.html',
-    styleUrls: ['./upload-image-post.component.css'],
+  selector: 'app-upload-image-post',
+  templateUrl: './upload-image-post.component.html',
+  styleUrls: ['./upload-image-post.component.css'],
 })
 export class UploadImagePostComponent {
-    @Output() public bodyToSend: EventEmitter<any> = new EventEmitter();
+  @Output() public bodyToSend: EventEmitter<any> = new EventEmitter();
 
-    imageChangedEvent: any = '';
-    croppedImage: any = '';
-    showCropper = false;
+  public imageChangedEvent: any = '';
+  public croppedImage: any = '';
+  public showCropper = false;
 
-    @ViewChild(ImageCropperComponent, null) imageCropper: ImageCropperComponent;
+  @ViewChild(ImageCropperComponent, null) imageCropper: ImageCropperComponent;
 
-    fileChangeEvent(event: any): void {
-        this.imageChangedEvent = event;
+  constructor(private readonly notificator: NotificatorService) {}
+
+  fileChangeEvent(event: any): void {
+    const file = event.target.files[0];
+
+    if (file.size > 10000000) {
+      this.notificator.error('File size is over the limit (maximum size 10MB)');
+      this.imageChangedEvent = '';
+      return;
     }
-    imageCropped(event: ImageCroppedEvent) {
-        this.croppedImage = event.base64;
-        this.bodyToSend.emit(this.croppedImage);
+
+    if (!file || !/image\/(gif|jpg|jpeg|png)$/i.test(file.type)) {
+      this.notificator.error('Invalid file format');
+      this.imageChangedEvent = '';
+      return;
     }
-    imageLoaded() {
-        this.showCropper = true;
-    }
-    cropperReady() {
-    }
-    loadImageFailed() {
-    }
-    rotateLeft() {
-        this.imageCropper.rotateLeft();
-    }
-    rotateRight() {
-        this.imageCropper.rotateRight();
-    }
-    flipHorizontal() {
-        this.imageCropper.flipHorizontal();
-    }
-    flipVertical() {
-        this.imageCropper.flipVertical();
-    }
+
+    this.imageChangedEvent = event;
+  }
+  imageCropped(event: ImageCroppedEvent) {
+    this.croppedImage = event.base64;
+    this.bodyToSend.emit(this.croppedImage);
+  }
+  imageLoaded() {
+    this.showCropper = true;
+  }
+  cropperReady() {}
+  loadImageFailed() {}
+  rotateLeft() {
+    this.imageCropper.rotateLeft();
+  }
+  rotateRight() {
+    this.imageCropper.rotateRight();
+  }
+  flipHorizontal() {
+    this.imageCropper.flipHorizontal();
+  }
+  flipVertical() {
+    this.imageCropper.flipVertical();
+  }
 }
