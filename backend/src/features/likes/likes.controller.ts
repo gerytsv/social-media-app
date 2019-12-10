40644b403d49cf92c843650635ab24f308ce5d5a
Gergana Tsvetkova <gerytsv@gmail.com>
2019-12-10 14:14:19 +0200

    Added functions to the likes controller

--- backend/src/features/likes/likes.controller.ts
+++ backend/src/features/likes/likes.controller.ts
@@ -23,4 +23,22 @@ export class LikesController {
   public async likes(@Param('postId') postId: string, @Request() request: any) {
     return await this.likesService.like(request.user.id, postId);
   }
+
+  @Get('/posts/:postId/likes')
+  @UseGuards(AuthGuard())
+  public async getAllLikes(
+    @Param('postId') postId: string,
+    @Request() request: any,
+  ) {
+    return {
+      votes: await this.likesService.getLikes(postId),
+      myVotes: await this.likesService.myLikes(postId, request.user.id),
+    };
+  }
+
+  @Get('/posts/:postId/likes/users')
+  @UseGuards(AuthGuard())
+  public async getAllUsersWhoLiked(@Param('postId') postId: string) {
+    return await this.likesService.getAllUsersLiked(postId);
+  }
 }
