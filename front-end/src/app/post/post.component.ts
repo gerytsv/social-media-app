f933e728daa893a10a056264d99a7e7f0923263e
Gergana Tsvetkova <gerytsv@gmail.com>
2019-12-10 17:35:47 +0200

    Marrying likes to  the post

--- front-end/src/app/post/post.component.ts
+++ front-end/src/app/post/post.component.ts
@@ -1,3 +1,4 @@
+import { PostsDataService } from './services/posts-data.service';
 import { Component, OnInit, Input } from '@angular/core';
 import { PostDTO } from './models/post.dto';
 import * as moment from 'moment';
@@ -9,9 +10,34 @@ import * as moment from 'moment';
 export class PostComponent implements OnInit {
   @Input() public post: PostDTO;
   public dateOfPost: string;
-  constructor() {}
+  public likes = { likes: 0 };
+  public myLike = false; // { liked: false };
+
+  constructor(private readonly postsDataService: PostsDataService) {}
 
   ngOnInit() {
     this.dateOfPost = moment(new Date()).format('MMM Do YY');
+
+    this.postsDataService.getLikesOfPost(this.post.id).subscribe(res => {
+      this.likes = res.likes;
+      this.myLike = res.myLikes.isLiked;
+
+      console.log('this.likes: ', this.likes);
+      console.log('res.myLikes.isLiked: ', res.myLikes.isLiked);
+      console.log('this.myLike: ', this.myLike);
+    });
+  }
+
+  public likePost() {
+    this.postsDataService.likePost(this.post.id).subscribe(res => {
+      this.postsDataService.getLikesOfPost(this.post.id).subscribe(res => {
+        this.likes.likes = res.likes.likes;
+        this.myLike = res.myLikes.isLiked;
+
+        console.log('this.likes: ', this.likes);
+        console.log('res.myLikes.isLiked: ', res.myLikes.isLiked);
+        console.log('this.myLike: ', this.myLike);
+      });
+    });
   }
 }
