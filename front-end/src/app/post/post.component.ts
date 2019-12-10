d0d7c42c3c01d0624d9824b091025b5810cc81d9
Gergana Tsvetkova <gerytsv@gmail.com>
2019-12-10 11:09:29 +0200

    Added newsfeed's post look

--- front-end/src/app/post/post.component.ts
+++ front-end/src/app/post/post.component.ts
@@ -1,14 +1,17 @@
-import { Component, OnInit, Input } from "@angular/core";
-import { PostDTO } from "./models/post.dto";
-
+import { Component, OnInit, Input } from '@angular/core';
+import { PostDTO } from './models/post.dto';
+import * as moment from 'moment';
 @Component({
-  selector: "app-post",
-  templateUrl: "./post.component.html",
-  styleUrls: ["./post.component.css"]
+  selector: 'app-post',
+  templateUrl: './post.component.html',
+  styleUrls: ['./post.component.css'],
 })
 export class PostComponent implements OnInit {
   @Input() public post: PostDTO;
+  public dateOfPost: string;
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.dateOfPost = moment(new Date()).format('MMM Do YY');
+  }
 }
