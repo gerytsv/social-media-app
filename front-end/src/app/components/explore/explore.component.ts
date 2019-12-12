eed57a4559071833f518a1e55b0b48aac8e5fd82
Gergana Tsvetkova <gerytsv@gmail.com>
2019-12-12 20:17:08 +0200

    Adding message if there are no posts to show

--- front-end/src/app/components/explore/explore.component.ts
+++ front-end/src/app/components/explore/explore.component.ts
@@ -5,32 +5,34 @@ import { ReplaySubject, Subject, Observable } from 'rxjs';
 import { Post } from '../../common/post';
 
 @Component({
-    selector: 'app-explore',
-    templateUrl: './explore.component.html',
-    styleUrls: ['./explore.component.css'],
+  selector: 'app-explore',
+  templateUrl: './explore.component.html',
+  styleUrls: ['./explore.component.css'],
 })
 export class ExploreComponent implements OnInit {
-    public posts: PostDTO[] = [];
-    public activePost: Post = null;
+  public posts: PostDTO[] = [];
+  public activePost: Post = null;
 
-    constructor(
-        private route: ActivatedRoute,
-        private readonly router: Router
-    ) {}
+  constructor(private route: ActivatedRoute, private readonly router: Router) {}
 
-    ngOnInit() {
-        this.route.data.subscribe(({ posts }) => {
-            posts.posts.subscribe(postsArray => {
-                this.posts = postsArray;
-                // console.log(this.posts);
-                if (this.posts.length === 0) {
-                    console.log('No posts to show!');
-                }
-            });
-        });
-    }
+  ngOnInit() {
+    this.route.data.subscribe(({ posts }) => {
+      posts.posts.subscribe(postsArray => {
+        this.posts = postsArray;
+        // console.log(this.posts);
+        if (this.posts.length === 0) {
+          let paragraphNoPosts = document.createElement('P');
+          paragraphNoPosts.innerText = 'No posts to show!';
+          document
+            .getElementById('posts-container')
+            .appendChild(paragraphNoPosts);
+          // console.log('No posts to show!');
+        }
+      });
+    });
+  }
 
-    // public selectPost(post: Post) {
-    //   this.router.navigate(["explore/posts", post.id]);
-    // }
+  // public selectPost(post: Post) {
+  //   this.router.navigate(["explore/posts", post.id]);
+  // }
 }
