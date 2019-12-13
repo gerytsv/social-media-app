5b4d864db1914bcc4292be7806bd2982abeac071
Georgi Krastev <gkrastev9715@abv.bg>
2019-12-13 20:29:50 +0200

    adding pagination to methods

--- front-end/src/app/core/resolvers/newsfeed.resolver.ts
+++ front-end/src/app/core/resolvers/newsfeed.resolver.ts
@@ -1,12 +1,12 @@
-import { Resolve } from "@angular/router";
-import { PostsDataService } from "../../post/services/posts-data.service";
-import { Injectable } from "@angular/core";
+import { Resolve } from '@angular/router';
+import { PostsDataService } from '../../post/services/posts-data.service';
+import { Injectable } from '@angular/core';
 
 @Injectable()
 export class NewsfeedResolver implements Resolve<any> {
   constructor(private readonly postsService: PostsDataService) {}
 
   resolve() {
-    return { posts: this.postsService.followedPosts() };
+    return { posts: this.postsService.followedPosts(5, 0) };
   }
 }
