c1974e99d3e8a231bb39e04c05b4dbe5b78cb05d
Georgi Krastev <gkrastev9715@abv.bg>
2019-12-13 11:41:34 +0200

    fixing post component static date

--- front-end/src/app/post/post.component.ts
+++ front-end/src/app/post/post.component.ts
@@ -11,32 +11,24 @@ export class PostComponent implements OnInit {
   @Input() public post: PostDTO;
   public dateOfPost: string;
   public likes = { likes: 0 };
-  public myLike = false; // { liked: false };
+  public myLike = false;
 
   constructor(private readonly postsDataService: PostsDataService) {}
 
   ngOnInit() {
-    this.dateOfPost = moment(new Date()).format('MMM Do YY');
+    this.dateOfPost = moment(this.post.postedOn).format('MMM Do YY (HH:MM)');
 
     this.postsDataService.getLikesOfPost(this.post.id).subscribe(res => {
       this.likes = res.likes;
       this.myLike = res.myLikes.isLiked;
-
-      // console.log('this.likes: ', this.likes);
-      // console.log('res.myLikes.isLiked: ', res.myLikes.isLiked);
-      // console.log('this.myLike: ', this.myLike);
     });
   }
 
   public likePost() {
-    this.postsDataService.likePost(this.post.id).subscribe(res => {
-      this.postsDataService.getLikesOfPost(this.post.id).subscribe(res => {
+    this.postsDataService.likePost(this.post.id).subscribe(response => {
+      this.postsDataService.getLikesOfPost(this.post.id).subscribe( res => {
         this.likes.likes = res.likes.likes;
         this.myLike = res.myLikes.isLiked;
-
-        // console.log('this.likes: ', this.likes);
-        // console.log('res.myLikes.isLiked: ', res.myLikes.isLiked);
-        // console.log('this.myLike: ', this.myLike);
       });
     });
   }
