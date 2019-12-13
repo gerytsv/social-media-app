19100c5735e0a79a9d26a4ea3ca5f8be95941d2e
Gergana Tsvetkova <gerytsv@gmail.com>
2019-12-13 13:28:33 +0200

    Removing unnecessary code

--- front-end/src/app/post/post.component.ts
+++ front-end/src/app/post/post.component.ts
@@ -10,34 +10,10 @@ import * as moment from 'moment';
 export class PostComponent implements OnInit {
   @Input() public post: PostDTO;
   public dateOfPost: string;
-  public likes = { likes: 0 };
-  public myLike = false; // { liked: false };
 
   constructor(private readonly postsDataService: PostsDataService) {}
 
   ngOnInit() {
     this.dateOfPost = moment(new Date()).format('MMM Do YY');
-
-    this.postsDataService.getLikesOfPost(this.post.id).subscribe(res => {
-      this.likes = res.likes;
-      this.myLike = res.myLikes.isLiked;
-
-      // console.log('this.likes: ', this.likes);
-      // console.log('res.myLikes.isLiked: ', res.myLikes.isLiked);
-      // console.log('this.myLike: ', this.myLike);
-    });
-  }
-
-  public likePost() {
-    this.postsDataService.likePost(this.post.id).subscribe(res => {
-      this.postsDataService.getLikesOfPost(this.post.id).subscribe(res => {
-        this.likes.likes = res.likes.likes;
-        this.myLike = res.myLikes.isLiked;
-
-        // console.log('this.likes: ', this.likes);
-        // console.log('res.myLikes.isLiked: ', res.myLikes.isLiked);
-        // console.log('this.myLike: ', this.myLike);
-      });
-    });
   }
 }
