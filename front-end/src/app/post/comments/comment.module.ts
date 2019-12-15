b0537b1bccb1e2d2a0c42e4995894a59e0d85df9
Georgi Krastev <gkrastev9715@abv.bg>
2019-12-15 16:13:37 +0200

    adding comments under post component

--- front-end/src/app/post/comments/comment.module.ts
+++ front-end/src/app/post/comments/comment.module.ts
@@ -5,15 +5,17 @@ import { CommentComponent } from './comment/comment.component';
 import { CommentsDataService } from './comments-data.service';
 import { CreateCommentComponent } from './create-comment/create-comment.component';
 import { AllCommentsComponent } from './all-comments/all-comments.component';
+import { CommentsUnderPostComponent } from './comments-under-post/comments-under-post.component';
 
 @NgModule({
     declarations: [
         CommentComponent,
         CreateCommentComponent,
         AllCommentsComponent,
+        CommentsUnderPostComponent
     ],
     imports: [CommonModule, FormsModule],
     providers: [CommentsDataService],
-    exports: [CommentComponent, CreateCommentComponent, AllCommentsComponent],
+    exports: [CommentComponent, CreateCommentComponent, AllCommentsComponent, CommentsUnderPostComponent],
 })
 export class CommentModule {}
