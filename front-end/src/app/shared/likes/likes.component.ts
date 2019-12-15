8be8311076510875bf8a484a438f1f10a15f8bf7
Gergana Tsvetkova <gerytsv@gmail.com>
2019-12-15 14:00:15 +0200

    Editing likes functionality

--- front-end/src/app/shared/likes/likes.component.ts
+++ front-end/src/app/shared/likes/likes.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit, Input } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  OnChanges,
+  DoCheck,
+  AfterViewInit,
+} from '@angular/core';
 import { PostsDataService } from '../../post/services/posts-data.service';
 import { PostDTO } from '../../post/models/post.dto';
 
@@ -7,7 +14,7 @@ import { PostDTO } from '../../post/models/post.dto';
   templateUrl: './likes.component.html',
   styleUrls: ['./likes.component.css'],
 })
-export class LikesComponent implements OnInit {
+export class LikesComponent implements OnInit, AfterViewInit {
   @Input() public post: PostDTO;
   public likes = { likes: 0 };
   public myLike = false;
@@ -15,7 +22,7 @@ export class LikesComponent implements OnInit {
   constructor(private readonly postsDataService: PostsDataService) {}
 
   public likePost() {
-    this.postsDataService.likePost(this.post.id).subscribe(res => {
+    this.postsDataService.likePost(this.post.id).subscribe(r => {
       this.postsDataService.getLikesOfPost(this.post.id).subscribe(res => {
         this.likes.likes = res.likes.likes;
         this.myLike = res.myLikes.isLiked;
@@ -23,10 +30,26 @@ export class LikesComponent implements OnInit {
     });
   }
 
+  public getNumberOfLikes() {
+    return this.likes.likes;
+  }
+
   ngOnInit() {
     this.postsDataService.getLikesOfPost(this.post.id).subscribe(res => {
       this.likes = res.likes;
       this.myLike = res.myLikes.isLiked;
     });
+  }
+
+  private changeView() {
+    this.postsDataService.getLikesOfPost(this.post.id).subscribe(res => {
+      this.likes = res.likes;
+      this.myLike = res.myLikes.isLiked;
+    });
+  }
+
+  ngAfterViewInit() {
+    console.log('in');
+    this.changeView();
   }
 }
