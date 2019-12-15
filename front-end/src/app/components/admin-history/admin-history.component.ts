4fa9c15eba474c51b32af5cce413bf2e7d848a01
Georgi Krastev <gkrastev9715@abv.bg>
2019-12-15 17:28:46 +0200

    canceling interval on destroy

--- front-end/src/app/components/admin-history/admin-history.component.ts
+++ front-end/src/app/components/admin-history/admin-history.component.ts
@@ -1,22 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { AdminDataService } from './admin-data.service';
 import { ShowHistoryDTO } from './models/show-history-dto';
+import { TimeInterval } from 'rxjs/internal/operators/timeInterval';
 
 @Component({
   selector: 'app-admin-history',
   templateUrl: './admin-history.component.html',
   styleUrls: ['./admin-history.component.css']
 })
-export class AdminHistoryComponent implements OnInit {
+export class AdminHistoryComponent implements OnInit, OnDestroy {
 
   constructor(private readonly adminDataService: AdminDataService) { }
 
   public history: ShowHistoryDTO[];
+  public interval: any;
 
   ngOnInit() {
     this.adminDataService.getHistory().subscribe(res => this.history = res.reverse());
-    setInterval(() => {
+    this.interval = setInterval(() => {
       this.adminDataService.getHistory().subscribe(res => this.history = res.reverse()); }, 4000);
-    }
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.interval);
+  }
 
 }
